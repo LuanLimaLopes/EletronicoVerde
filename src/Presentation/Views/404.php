3eab5a6de4728e79c87b41649fe9ba08666efc4a
Luan Lima Lopes <luandodds543@gmail.com>
2025-11-08 02:31:21 -0300

    Fix: linkagem

--- src/Presentation/Views/404.php
+++ src/Presentation/Views/404.php
@@ -18,7 +18,7 @@
         <h1 class="text-[50em] absolute font-bold opacity-20 left-0 top-0 text-primary -z-2">404</h1>
         <h1 class="text-6xl font-bold italic">Página não encontrada</h1>
         <p class="text-3xl ">A página que você está procurando não existe.</p>
-        <a href="<?= BASE_URL ?>/index.php" class="bg-primary text-2xl text-white p-4 w-fit h-fit rounded-3xl rounded-tl-none border-2 border-primary relative overflow-hidden z-1
+        <a href="/eletronicoverde" class="bg-primary text-2xl text-white p-4 w-fit h-fit rounded-3xl rounded-tl-none border-2 border-primary relative overflow-hidden z-1
         hover:rounded-tr-sm hover:rounded-tl-3xl hover:text-primary transition-all duration-150
         before:absolute before:h-full before:-z-1 before:w-0 hover:before:w-full before:bg-white before:bottom-0 before:left-0 before:transition-all before:duration-250">
         Voltar para a página inicial</a>
